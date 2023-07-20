acb5307ff0a6b3067f05cc03894ab4ab80bd9174
Soheil Asghari <soheil.developer@gmail.com>
2023-07-20 21:11:36 +0330

    Create iptables_email_block.sh

---
+++ iptables_email_block.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sysctl net.ipv4.ip_forward=1
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -A OUTPUT -p tcp --dport 25 -j DROP
+iptables -A INPUT -p tcp --dport 25 -j DROP
+
+iptables-save > /etc/iptables/rules.v4
+
+if ! grep -q "iptables_email_block.sh" /etc/rc.local; then
+  echo "bash /root/iptables_email_block.sh" >> /etc/rc.local
+fi
